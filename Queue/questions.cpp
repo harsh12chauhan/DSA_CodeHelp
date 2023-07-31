a2ed5120a3795febb1468fc9060c14eb5aa6f133
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-07-31 18:13:38 +0530

    First negative integer in every window of size k

--- Queue/questions.cpp
+++ Queue/questions.cpp
@@ -42,23 +42,67 @@ void qPrint(queue<int> q){
     }
     cout<<endl<<"queue is printed "<<endl;
 }
+
+//function First negative integer in every window of size k
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
+    
+    deque<long long int> dq;
+    vector<long long> ans;
+    for(int i = 0;i<K;i++){
+        if(A[i]<0){
+            dq.push_back(i);
+        }
+    }
+    //storing answer
+    if(dq.size() > 0){
+        ans.push_back(A[dq.front()]);
+    }else{
+        ans.push_back(0);
+    }
+    //process for remaining window
+    for(int i = K;i<N;i++){
+        //removal
+        if(!dq.empty() && i-dq.front() >= K){
+            dq.pop_front();
+        }
+        //addition
+        if(A[i] < 0){
+            dq.push_back(i);
+        }
+        //storing answer
+        if(dq.size() > 0){
+            ans.push_back(A[dq.front()]);
+        }else{
+            ans.push_back(0);
+        }
+    }
+    return ans;
+}
 int main(){
     queue<int> q;
-    q.push(6);
-    q.push(9);
-    q.push(7);
-    q.push(5);
-    q.push(1);
-    qPrint(q);
+    // q.push(6);
+    // q.push(9);
+    // q.push(7);
+    // q.push(5);
+    // q.push(1);
+    // qPrint(q);
 //reverse queue using stack
     // q = rev(q);    
     // qPrint(q);
 
 //reverse queue using recursion
-    recursionRev(q);  
-    qPrint(q);
+    // recursionRev(q);  
+    // qPrint(q);
 
-
+// First negative integer in every window of size k
+long long int N = 5;
+long long int A[] = {-8, 2, 3, -6, 10};
+long long int K = 2;
+vector<long long> v = printFirstNegativeInteger(A,N,K);
+cout<<"negative integers are -> "<<endl;
+for(int i =0;i<v.size();i++){
+    cout<<v[i]<<" ";
+}cout<<endl;
 
 return 0;
 }
