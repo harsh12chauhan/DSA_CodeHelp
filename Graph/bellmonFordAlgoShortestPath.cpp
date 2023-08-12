c7137d0d9f1978b9774f4116ad9b13499b0e3188
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-08-12 18:53:02 +0530

    Bellmonford Algorithm for finding shortest path with negative weights

---
+++ Graph/bellmonFordAlgoShortestPath.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+   
+    vector<int> dist(n+1,1e9); 
+    dist[src] = 0;
+
+    // n-1 times
+    for(int i=1;i<n;i++){
+        //traverse on edge list
+        for(int j=0;j<m;j++){
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int w = edges[j][2];
+            
+            //formula of bellmonFord
+            if(dist[u] != 1e9 && (dist[u]+ w < dist[v])){
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    //check cycle is present or not
+    bool flag = false;
+    for(int j=0;j<m;j++){
+        int u = edges[j][0];
+        int v = edges[j][1];
+        int w = edges[j][2];
+        
+        //formula of bellmonFord
+        if(dist[u] != 1e9 && (dist[u]+ w < dist[v])){
+            flag = true;
+        }
+    }
+    
+    if(!flag){
+        return dist[dest];
+    }else{
+        return -1;
+    }
+}
+
+int main(){
+vector<vector<int>> edges{{1, 2, 2},
+                          {1, 3, 2},
+                          {2, 3, -1}
+};
+int n = 3;
+int m = 3;
+int src = 1;
+int dest = 3;
+int ans = bellmonFord(n,m,src,dest,edges);
+cout<<ans<<endl;
+return 0;
+}
